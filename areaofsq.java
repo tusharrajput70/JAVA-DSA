5e2ecc3917c0fb329031b61cedbcbfe9c4bda114
TUSHAR RAJPUT <114666939+tusharrajput70@users.noreply.github.com>
2023-02-20 11:23:54 +0530

    Add files via upload

---
+++ areaofsq.java
@@ -0,0 +1,15 @@
+package basic;
+import java.util.Scanner;
+
+public class areaofsq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("enter the side of square");
+		Scanner sc= new Scanner(System.in);
+		float a=sc.nextFloat();
+		float area=a*a;
+		System.out.print("area ="+area);
+	}
+	
+}
